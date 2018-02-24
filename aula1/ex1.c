0ba20a71718a7b66b1ca43be81769a47f1fb6119
jonatas57 <jdjonatasduarte57@gmail.com>
2018-02-24 18:54:20 -0300

    finalizado

--- aula1/ex1.c
+++ aula1/ex1.c
@@ -1,11 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-  unsigned long long int n;
-  char hex[17] = "";
-  scanf("%d", &n);
-  sprintf(hex, "%llx", n);
-  printf("%llx\n", n);
-  printf("[%s]\n", hex);
+  int k, m, *s;
+  scanf("%d %d", &m, &k);
+  s = (int *)malloc(m * sizeof(int));
+  for (int i = 0;i < m;i++){
+    scanf("%d", s + i);
+  }
+  int p = 0;
+  for(int i = 0, j = 1;j <= m;j++){
+    if (s[j] == s[j - 1] + 1){
+      p = 1;
+    }
+    else {
+      if (p) {
+        p = 0;
+        if (j - i <= k) {
+          for(int l = i;l < j;l++){
+            printf("%d ", s[l]);
+          }
+          printf("\n");
+        }
+        else {
+          for (int a = i;a <= j - k;a++){
+            for (int b = 0;b < k;b++){
+              printf("%d ", s[a + b]);
+            }
+            printf("\n");
+          }
+        }
+      }
+      i = j;
+    }
+  }
   return 0;
 }
