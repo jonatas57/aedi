4b6cf8363c2c322ce9c08831b50a8e04d713f0c7
jonatas57 <jdjonatasduarte57@gmail.com>
2018-04-28 01:03:26 +0000

    funcionando

--- iam/autreta.cpp
+++ iam/autreta.cpp
@@ -2,8 +2,14 @@
 #include <cstdlib>
 #include <cstdio>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+typedef vector<double> vdouble;
+typedef vector<string> vstring;
+typedef vector<double>::iterator dit;
+typedef vector<string>::iterator sit;
 
 class polinomio {
 private:
@@ -19,14 +25,39 @@ public:
 			cin >> coef[i];
 		}
 	}
-	void ler(double *aux) {
+	void ler(vdouble aux) {
 		for (int i = 0;i <= grau;i++) {
 			coef[i] += aux[i];
 		}
 	}
 	void print() {
-		for (int i = 0;i <= grau;i++) {
-			printf("%.0f ", coef[i]);
+		bool p = false;
+		for (int i = grau;i >= 0;i--) {
+			string m = "";
+			if (coef[i] != 0) {
+				if ((coef[i] > 0 && i < grau) && p) {
+					cout << "+";
+				}
+				if (coef[i] != 1 || i == 0) {
+					if (coef[i] == -1) {
+						m += "-";
+					}
+					else {
+						char *aux = new char[30];
+						sprintf(aux, "%.0f", coef[i]);
+						m += aux;
+					}
+				}
+				if (i >= 1) {
+					m += "x";
+				}
+				if (i > 1) {
+					m += "^";
+					m += to_string(i);
+				}
+			}
+			cout << m;
+			if (m != "") p = true;
 		}
 		cout << endl;
 	}
@@ -58,42 +89,77 @@ public:
 		return res;
 	}
 };
-string* getMono(string s, int l) {
-	string *m = new string[l];
-	int i = 0, last = 0, c = 0;
+
+void check() {
+	cout << "check" << endl;
+}
+
+vstring getMono(string s, int l) {
+	vstring m;
+	int i = 0, last = 0;
 	do {
 		i++;
 		if (s[i] == '+' || s[i] == '-' || s[i] == '\0') {
-			m[c] = s.substr(last, i - last);
-			c++;
+			m.push_back(s.substr(last, i - last));
 			last = i;
 		}
 	} while (s[i] != '\0');
 	return m;
 }
-double* getNum(string *s, int l) {
-	
-	double *num = NULL;
+vdouble getNum(vstring s, int l) {
+	vdouble num;
+	for (sit it = s.begin();it != s.end();it++) {
+		string ss = *it;
+		int ls = ss.length();
+		int var;
+		string c, e = "0";
+		var = ss.find('x', 0);
+		if (var == -1) {
+			c = ss;
+		}
+		else if (var == 0) {
+			c = "1.0";
+		}
+		else if (var == 1 && !isdigit(ss[0])) {
+			c = ss[0];
+			c += "1.0";
+		}
+		else {
+			c = ss.substr(0, var);
+			if ((int)c.find('.', 0) == -1) c += ".0";
+		}
+		if (var == ls - 1) {
+			e = "1";
+		}
+		else if (var != - 1) {
+			e = ss.substr(var + 2);
+		}
+		int ie = stoi(e, nullptr, 10);
+		while (ie >= (int)num.size()) {
+			num.push_back(0);
+		}
+		*(num.begin() + ie) += stod(c, nullptr);
+	}
 	return num;
 }
 polinomio stop(string s, int l) {
-	string *ss = getMono(s, l);
-	double *aux = getNum(ss, l);
-	polinomio p(l - 1);
+	vdouble aux = getNum(getMono(s, l), l);
+	int t = aux.size();
+	polinomio p(t - 1);
 	p.ler(aux);
 	return p;
 }
 
 int main() {
-	string s, t, *ss;
+	string s;
 	cin >> s;
 	int l = 1;
 	for (int i = 1;s[i] != '\0';i++) {
-		if (s[i] == '+' || s[i] == '-') l++;
+		if (s[i] == '+' || s[i] == '-') {
+			l++;
+		}
 	}
-	ss = getMono(s, l);
-	for (int i = 0;i < l;i++) cout << ss[i] << endl;
-	
-	//polinomio p = stop(s, l);
+	polinomio p = stop(s, l);
+	p.print();
 	return 0;
 }
