6b6217842af19d07ffefe94631933cabc1ffb0af
jonatas57 <jdjonatasduarte57@gmail.com>
2018-05-03 11:56:40 -0300

    teste

--- git.sh
+++ git.sh
@@ -1,6 +1,10 @@
-git config --local user.name jonatas57
-git config --local user.email jdjonatasduarte57@gmail.com
+name= grep -r "name" ./.git/config | awk '{print $3}'
+email= grep -r "email" ./.git/config | awk '{print $3}'
+if [ "$name" -ne "jonatas57" ]; then
+  git config --local user.name jonatas57
+  git config --local user.email jdjonatasduarte57@gmail.com
 read -p "add: " files
 git add $files
 read -p "commit: " msg
 git commit -m $msg
+git push
