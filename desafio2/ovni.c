b373bc13f4535c7c7e551db4b3c09997452a289f
jonatas57 <jdjonatasduarte57?@gmail.com>
2018-05-05 00:52:36 +0000

    quicksort

--- desafio2/ovni.c
+++ desafio2/ovni.c
@@ -1,43 +1,106 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct linkNode ovni;
-struct linkNode {
+typedef struct linkNode linkNode;
+typedef struct ovni ovni;
+struct ovni {
 	int vel, cap, pra;
 	double ef;
 	char *mod;
+};
+struct linkNode {
+	ovni *data;
 	linkNode *next;
 };
 
-ovni* newNode(char *m, int v, double k, int c, int p);
-ovni* insertNodd(linkNode *list, linkNode *x);
-void printList(linkNode *list);
-int compare(a, b);
-void qsort();
-void sort(linkNode *list, int m);
-void freeList(linkNode *list);
+linkNode* newNode(char *m, int v, double k, int c, int p) {
+	linkNode *x = malloc(sizeof(linkNode));
+	ovni *o = malloc(sizeof(ovni));
+	if (x != NULL && o != NULL) {
+		o->mod = m;
+		o->vel = v;
+		o->ef = k;
+		o->cap = c;
+		o->pra = p;
+		x->data = o;
+		x->next = NULL;
+	}
+	return x;
+}
+linkNode* insertNode(linkNode *list, linkNode *last, linkNode *x) {
+	if (last == NULL) {
+		last = x;
+		return x;
+	}
+	last->next = x;
+	last = x;
+	return list;
+}
+void printList(linkNode *list) {
+	for (;list != NULL;list = list->next) {
+		printf("[M=%s V=%d KM-L=%.2f C=%d E=%d]\n", list->mod, list->vel, list->ef, list->cap, list->pra);
+	}
+}
+int compare(ovni a, ovni b, int m, int o) {
+	int comp;
+	if (m == 1) {
+		comp = strcmp(a->mod, b->mod);
+	}
+	else if (m == 2) {
+		comp = a->cap > b->cap ? 1 : -1;
+	}
+	else if (m == 5) {
+		if (a->pra > b->pra) comp = 1;
+		else if (a->pra < b->pra) comp = -1;
+		else {
+			comp * (a->ef > b->ef ? 1 : -1);
+			comp * (o ? 1 : -1);
+		}
+		return comp;
+	}
+	else if (m == 6) {
+		comp = a->pra > b->pra ? 1 : -1;
+	}
+	return comp * (o ? -1 : 1);
+}
+void quicksort(linkNode *list, linkNode *last, int m, int ord) {
+	
+}
+void sort(linkNode *list, linkNode *last, int m) {
+	printf("[O=%d]\n", m);
+	int ord = m % 2;
+	m /= 10;
+	quicksort(list, last, m, ord);
+	printList(list);
+}
+void freeList(linkNode *list) {
+	if (list == NULL) return;
+	freeList(list->next);
+	free(list);
+}
 
-int main() 
-	ovni *d = NULL;
+int main() {
+	linkNode *list = NULL, *last = NULL;
 	int op;
 	while (scanf("%d", &op) != -1) {
 		if (op == 1) {
 			char *m;
 			int v, c, p;
 			double k;
-			scanf("%s %d %f %d %d", &m, &v, &k, &c, &p);
-			d = insertNode(d, newNode(m, v, k, c, p);
+			scanf("%s %d %lf %d %d", m, &v, &k, &c, &p);
+			list = insertNode(list, last, newNode(m, v, k, c, p));
 		}
 		else if (op == 2) {
-			printList(d);
+			printf("[DISCOS]\n");
+			printList(list);
 		}
 		else if (op == 8) {
 			int m;
 			scanf("%d", &m);
-			sort(d, m);
+			sort(list, last, m);
 		}
 		else if (op == 0) {
-			freeList(d);
+			freeList(list);
 			return 0;
 		}
 	}
